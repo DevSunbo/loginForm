67f7866284d2f35326afa4ad0ae9acaf815a0133
DevSunbo <cowwhite1008@gmail.com>
2020-03-22 13:10:35 +0900

    routes에 login 추가

--- routes.js
+++ routes.js
@@ -1,10 +1,12 @@
 //Global
 const HOME = "/";
 const JOIN = "/join";
+const LOGIN = "/login";
 
 const routes = {
     home: HOME,
-    join: JOIN
+    join: JOIN,
+    login: LOGIN
 }
 
 export default routes;
