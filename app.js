f1c1296148cebc9b003e9dceb314052090581178
DevSunbo <cowwhite1008@gmail.com>
2020-03-27 20:25:35 +0900

    home router 추가 & db.js 옮겨 옴

--- app.js
+++ app.js
@@ -10,12 +10,22 @@ import session from "express-session";
 import MongoStore from "connect-mongo";
 import globalRouter from "./routers/globalRouter";
 import dotenv from "dotenv"
+import { localsMiddleware } from "./middlewares";
 
 dotenv.config();
 
 const app = express();
 
 const CookieStore = MongoStore(session);
+
+const connect = mongoose.connect(
+    process.env.MONGO_URL,
+    {
+        useNewUrlParser: true,
+        useFindAndModify: true
+    }
+).then(()=> console.log("connect to db"))
+.catch(err => console.log(err));
 
 app.use(helmet());
 app.use(cookieParser());
@@ -38,6 +48,10 @@ app.use(
 // passport가 스스로 쿠키를 들여다봐서 쿠키 정보에 해당하는 사용자를 찾아줌
 app.use(passport.initialize()); 
 app.use(passport.session());
+
+// app.use(localsMiddleware)
+
+app.use(routes.home, globalRouter); // 이 구문이 없으면 join 페이지 404에러 왜 필요한 것인지 모르겠음
 
 
 export default app;
